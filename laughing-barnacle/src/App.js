ab118d87ef4bac67789a1c4853b61e33a7f86f48
theCodeFox <akkitta@live.co.uk>
2020-01-21 22:15:24 +0000

    basic react app set-up with starter css grid

---
+++ laughing-barnacle/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-container">
+      <header className="App-header App-header-area">
+        EXAMPLE HEADER
+      </header>
+      <div className="App-left-area">
+        SUN IMAGE?
+      </div>
+      <div className="App-right-area">
+        RAIN IMAGE?
+      </div>
+      <div className="App-main-area">
+        MAIN TEXT AREA
+      </div>
+      <footer className="App-footer-area">
+        EXAMPLE FOOTER
+      </footer>
+    </div>
+  );
+}
+
+export default App;
