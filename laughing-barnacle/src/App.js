e5d600686dd996cfa8af4204fbbc745374ee1a00
theCodeFox <akkitta@live.co.uk>
2020-02-02 22:15:12 +0000

    footer links open in new tab

--- laughing-barnacle/src/App.js
+++ laughing-barnacle/src/App.js
@@ -15,13 +15,13 @@ function App() {
         MAIN TEXT AREA
       </div>
       <footer className="App-footer-area">
-        <a href="https://www.linkedin.com/in/kay-vose-codefox/">
+        <a href="https://www.linkedin.com/in/kay-vose-codefox/" target="_blank" rel="noopener noreferrer">
           <img className="footer-img" src={require("./images/linkedin-logo.png")} alt="linkedin icon" />
         </a>
-        <a href="https://codeandstuff-manchester.github.io/">
+        <a href="https://codeandstuff-manchester.github.io/" target="_blank" rel="noopener noreferrer">
           <img className="footer-img" src="https://d3jh33bzyw1wep.cloudfront.net/s3/W1siZiIsIjIwMTkvMDYvMTgvMTAvMDUvMTAvODYzL2NvZGUuUE5HIl0sWyJwIiwidGh1bWIiLCI2MDB4NDUwXHUwMDNlIl1d" alt="code and stuff icon" />
         </a>
-        <a href="https://github.com/theCodeFox">
+        <a href="https://github.com/theCodeFox" target="_blank" rel="noopener noreferrer">
           <img className="footer-img" src={require("./images/github-logo.png")} alt="github icon" />
         </a>
       </footer>
