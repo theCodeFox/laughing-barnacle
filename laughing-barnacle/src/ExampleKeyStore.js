114f9fe3584de8e6a1f2ce76a71e8001629ea2e3
theCodeFox <akkitta@live.co.uk>
2020-02-06 17:35:33 +0000

    example key store added

---
+++ laughing-barnacle/src/ExampleKeyStore.js
@@ -0,0 +1,10 @@
+const exampleKeyStore = {
+  Site: "https://home.openweathermap.org/",
+  Username: "Code is Cool",
+  Password: "abc123",
+  APIKey: "hereLiesYourAPIId",
+  Endpoint: "api.openweathermap.org",
+  APIURL: "https://api.openweathermap.org/data/2.5/forecast?q=Manchester,uk&APPID=hereLiesYourAPIId"
+};
+
+export default exampleKeyStore;
